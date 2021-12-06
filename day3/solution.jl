06f1391fa91a5f80fb8845c0a4f06a479ae23011
Zach Newton <znewton@bamboohealth.com>
2021-12-06 17:14:43 -0500

    day3 - p2

--- day3/solution.jl
+++ day3/solution.jl
@@ -41,8 +41,16 @@ end
 
 function bin_array_to_int(bin_array)
     output = 0
-    for i in length(bin_array):-1:1
-        output += bin_array[i] * 2^(length(bin_array)-i)
+    for i = length(bin_array):-1:1
+        output += bin_array[i] * 2^(length(bin_array) - i)
+    end
+    return output
+end
+
+function string_array_to_int(bin_array)
+    output = 0
+    for i = length(bin_array):-1:1
+        output += parse(Int, bin_array[i]) * 2^(length(bin_array) - i)
     end
     return output
 end
@@ -51,34 +59,73 @@ function calculate_product(val_array)
     eps_array = flip_bit(val_array)
     gamma = bin_array_to_int(val_array)
     epsilon = bin_array_to_int(eps_array)
-    return epsilon*gamma
+    return epsilon * gamma
 end
 
-function solution1(lines)
+
+function valmatrix(lines)
     line_length = nothing
     val_matrix = []
     for line in lines
         if isnothing(line_length)
             line_length = length(line)
-            for i in 1:line_length
+            for i = 1:line_length
                 append!(val_matrix, [[]])
             end
         end
-        for i in 1:line_length
+        for i = 1:line_length
             append!(val_matrix[i], line[i])
         end
 
     end
-
-    res = []
-    for i in 1:line_length
-        append!(res, val_matrix[i])
-    end
-    return calculate_product(mode(val_matrix))
-    
+    return val_matrix
 end
 
+function solution1(lines)
+    val_matrix = valmatrix(lines)
+    return calculate_product(mode(val_matrix))
+end
+
+
+
 function solution2(lines)
+    val_matrix = valmatrix(lines)
+    co2_rating = nothing
+    o2_rating = nothing
+    co2_rows = range(1, length(val_matrix[1]))
+    o2_rows = range(1, length(val_matrix[1]))
+    for i = 1:12
+
+        count_0 = length([val_matrix[i][j] for j in co2_rows if val_matrix[i][j] == '0'])
+        count_1 = length([val_matrix[i][j] for j in co2_rows if val_matrix[i][j] == '1'])
+        if count_0 > count_1
+            co2_rows = [j for j in co2_rows if val_matrix[i][j] == '1']
+        elseif count_1 > count_0
+            co2_rows = [j for j in co2_rows if val_matrix[i][j] == '0']
+        else
+            co2_rows = [j for j in co2_rows if val_matrix[i][j] == '0']
+        end
+        if length(co2_rows) == 1
+            co2_rating = string_array_to_int([col[co2_rows[1]] for col in val_matrix])
+        end
+    end
+
+    for i = 1:12
+
+        count_0 = length([val_matrix[i][j] for j in o2_rows if val_matrix[i][j] == '0'])
+        count_1 = length([val_matrix[i][j] for j in o2_rows if val_matrix[i][j] == '1'])
+        if count_0 > count_1
+            o2_rows = [j for j in o2_rows if val_matrix[i][j] == '0']
+        elseif count_1 > count_0
+            o2_rows = [j for j in o2_rows if val_matrix[i][j] == '1']
+        else
+            o2_rows = [j for j in o2_rows if val_matrix[i][j] == '1']
+        end
+        if length(o2_rows) == 1
+            o2_rating = string_array_to_int([col[o2_rows[1]] for col in val_matrix])
+        end
+    end
+    return o2_rating * co2_rating
 
 end
 
