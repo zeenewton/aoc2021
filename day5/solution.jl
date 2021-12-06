a6c8bb86ef6d6d355d23cdee9a2cebe5b6e0274d
Zach Newton <znewton@bamboohealth.com>
2021-12-06 15:13:06 -0500

    day5

--- day5/solution.jl
+++ day5/solution.jl
@@ -4,22 +4,32 @@ function solution1(lines)
     hit_map = Dict()
     for line in lines
         coords = []
+        step = 1
         for sub_block in split(line, " -> ")
-            append!(coords, [split(sub_block, ",")])
+            append!(coords, [parse(Int, x) for x in split(sub_block, ",")])
         end
-        # coords = [(x,y for (x,y) in split(sub_block, ",")) for sub_block in split(line, " -> ")]
-        if coords[1][1] == coords[2][1] 
-            for i in parse(Int,coords[1][2]):parse(Int,coords[2][2])
-                entry = string(coords[1][1],",", i) 
+
+        x_diff = coords[1] - coords[3]
+        y_diff = coords[2] - coords[4]
+
+        if x_diff == 0
+            if coords[2] > coords[4]
+                step = -1
+            end
+            for i = coords[2]:step:coords[4]
+                entry = string(coords[1], ",", i)
                 if entry in keys(hit_map)
                     hit_map[entry] += 1
                 else
                     hit_map[entry] = 1
                 end
             end
-        elseif coords[1][2] == coords[2][2]
-            for i in parse(Int,coords[1][1]):parse(Int,coords[2][1])
-                entry = string(i, ",", coords[1][2]) 
+        elseif y_diff == 0
+            if coords[1] > coords[3]
+                step = -1
+            end
+            for i = coords[1]:step:coords[3]
+                entry = string(i, ",", coords[2])
                 if entry in keys(hit_map)
                     hit_map[entry] += 1
                 else
@@ -27,11 +37,70 @@ function solution1(lines)
                 end
             end
         else
-            println("SKIP")
+            println("skip")
         end
     end
-    println(hit_map)
-    return size([k for (k,v) in hit_map if v >= 2])
+    return length([k for (k, v) in hit_map if v >= 2])
+end
+
+function solution2(lines)
+    hit_map = Dict()
+    for line in lines
+        coords = []
+        step = 1
+        for sub_block in split(line, " -> ")
+            append!(coords, [parse(Int, x) for x in split(sub_block, ",")])
+        end
+
+        x_diff = coords[1] - coords[3]
+        y_diff = coords[2] - coords[4]
+
+        if x_diff == 0
+            if coords[2] > coords[4]
+                step = -1
+            end
+            for i = coords[2]:step:coords[4]
+                entry = string(coords[1], ",", i)
+                if entry in keys(hit_map)
+                    hit_map[entry] += 1
+                else
+                    hit_map[entry] = 1
+                end
+            end
+        elseif y_diff == 0
+            if coords[1] > coords[3]
+                step = -1
+            end
+            for i = coords[1]:step:coords[3]
+                entry = string(i, ",", coords[2])
+                if entry in keys(hit_map)
+                    hit_map[entry] += 1
+                else
+                    hit_map[entry] = 1
+                end
+            end
+        elseif abs(x_diff) == abs(y_diff)
+            y_step = 1
+            if x_diff > 0
+                step = -1
+            end
+            if y_diff > 0
+                y_step = -1
+            end
+            for (x_pos, y_pos) in zip(coords[1]:step:coords[3], coords[2]:y_step:coords[4])
+                entry = string(x_pos, ",", y_pos)
+                if entry in keys(hit_map)
+                    hit_map[entry] += 1
+                else
+                    hit_map[entry] = 1
+                end
+            end
+        else
+            println("skip")
+        end
+    end
+    return length([k for (k, v) in hit_map if v >= 2])
 end
 
 println(solution1(lines))
+println(solution2(lines))
